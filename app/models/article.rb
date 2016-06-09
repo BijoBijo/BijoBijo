030ffef04c78f995f4b17a7656b29aa11d2dec43
Seo Kyohei <Kyohei@Seo.local>
2016-06-09 20:30:04 +0900

    Set enum about of status to release or preview article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
+
+  enum status: { release: 0, preview: 1 }
 end

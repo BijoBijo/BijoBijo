6af8432148a8cac646eecc974c55951e924cac41
Seo Kyohei <Kyohei@Seo.local>
2016-06-09 20:30:48 +0900

    Revert "Set enum about of status to release or preview article"
    This reverts commit 030ffef04c78f995f4b17a7656b29aa11d2dec43.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,4 @@
 class Article < ActiveRecord::Base
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-
-  enum status: { release: 0, preview: 1 }
 end
